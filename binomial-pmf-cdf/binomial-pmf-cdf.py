3140f4231ddae8300853175247064e72509492e4
Tiyamisu <tiyasha.sarkar31@gmail.com>
2026-07-24 11:24:04 +0530

    Solve binomial-pmf-cdf

---
+++ binomial-pmf-cdf/binomial-pmf-cdf.py
@@ -0,0 +1,15 @@
+import numpy as np
+from scipy.special import comb
+
+def binomial_pmf_cdf(n, p, k):
+    """
+    Compute Binomial PMF and CDF.
+    """
+
+    pmf = comb(n, k) * (p ** k) * ((1 - p) ** (n - k))
+
+    cdf = 0.0
+    for i in range(k + 1):
+        cdf += comb(n, i) * (p ** i) * ((1 - p) ** (n - i))
+
+    return (float(pmf), float(cdf))
